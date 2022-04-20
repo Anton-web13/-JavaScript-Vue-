cc2b94698aaea4d1a3d86a30c38d2cfb416fbfdb
Anton <example@gmail.com>
2022-04-20 19:06:04 +0300

    9. Числа

--- js/app.js
+++ js/app.js
@@ -1,62 +1,22 @@
 let value;
-
-// Number to string
-
-value = 10;
-console.log(value);
-console.log(typeof value);
-
-value = String(40+10);
-value = (40).toString();
-console.log(value);
-console.log(typeof value);
-
-console.clear();
-
-// value - String(10);
-
-// value = String(10);
-// value = String(10+40);
-// value = (40).toString();
-
-// Boolean to string
-
-value = String(null);
-
-// Array to string
-
-value = String([1, 2, 3]);
-value = String({name: "Denis"});
-
-value = 30 + '' + 30;
-value = 30 - 'dfg';
-value = 30 - '5';
-value = true + 10;
-
-// String to number
-
-value = Number('23');
-value = Number(true);
-value = Number(false);
-value = Number(null);
-value = Number('false');
-value = Number([1, 2, 3]);
-
-value = parseInt('200zsfrzsrf');
-value = parseInt('200');
-value = parseFloat('200.12chgjchg');
-
-// Boolean
-
-value = Boolean('sdfsdf');
-value = Boolean('');
-value = Boolean(-100);
-value = Boolean(0);
-value = Boolean(undefined);
-value = Boolean(null);
-value = Boolean({});
-value = Boolean([]);
+value = 0.6+0.7;
+// value = parseFloat(value.toFixed(1));
+value = (0.6*10 + 0.7*10)/10;
 
 
-console.log(value);
-console.log(typeof value);
+// Math
+
+value = Math.PI;
+value = Math.random();
+value = Math.round(2.4);
+value = Math.ceil(2.4);
+value = Math.floor(2.4);
+value = Math.min(2, 15, 1, 45);
+
+value = Math.floor(Math.random() * 10 + 1);
+
+const arr = ["black", 'red', 'yellow', 'pink', 'white', 'blue', 'orange'];
+value = Math.floor(Math.random() * arr.length);
+
+
+console.log(value, arr[value]);  
