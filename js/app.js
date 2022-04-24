d1df8ab704929600c0fa1c46145f5802e90e70e2
Anton <example@gmail.com>
2022-04-24 20:08:49 +0300

    Числа. Задачи.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
-let string = 'some test string';
-
 let value;
-value = string[0];
-value = string[string.length - 1];
-value = string[0].toUpperCase();
-value = string[string.length - 1].toUpperCase();
-value = string.indexOf('string');
-value = string.indexOf(' ', string.indexOf('test'));
-value = string.slice(5, 9);
-value = string.slice(5, 9);
-value = string.slice(5, -5);
-let a = 20;
-let b = 16;
-value = '20' + '16';
-value = string.concat(' ', b);
-a = '20';
-b = '16';
-value = a.concat('', b);
+let x = 100;
+value = Math.PI;
+value = Math.ceil(2.4);
+value= Math.floor(Math.PI);
+value = Math.round(2.4);
+value = Math.floor(Math.random() * x.length);
+value = Math.PI.toPrecision(3);
+value = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+value = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+value = Math.random().toPrecision(2);
+value = Math.random() * x;
+value = Math.random().toPrecision(4) * x;
+let a = 0.6;
+let b = 0.7;
+value = a + b;
+// value = (a + b) * 1;
+// value = value.toFixed(1);
+value = parseFloat(value.toFixed(1));
+value = '100$';
+value = parseInt(value);
 
 
 console.log(value);
