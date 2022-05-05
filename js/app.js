e54c893c87a079582ad14d7c09eb44ece2f5d7d1
Anton <example@gmail.com>
2022-05-05 23:22:11 +0300

    14. Введение в объекты

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
+const user = {
+    firstName: 'Denis',
+    age: 30,
+    isAdmin: true,
+    email: 'test@test.com',
+    'user-address': {
+        city: 'Kharkiv'
+    },
+    skills: ['html', 'css', 'js']
+};
+
 let value;
-let x = 100;
-value = Math.PI;
-value = Math.ceil(2.4);
-value= Math.floor(Math.PI);
-value = Math.round(2.4);
-value = Math.floor(Math.random() * x.length);
-value = Math.PI.toPrecision(3);
-value = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
-value = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
-value = Math.random().toPrecision(2);
-value = Math.random() * x;
-value = Math.random().toPrecision(4) * x;
-let a = 0.6;
-let b = 0.7;
-value = a + b;
-// value = (a + b) * 1;
-// value = value.toFixed(1);
-value = parseFloat(value.toFixed(1));
-value = '100$';
-value = parseInt(value);
+let prop = 'skills';
+
+value = user.firstName;
+value = user['isAdmin'];
+value = user['user-address'];
+value = user['user-address'].city;
+value = user['user-address']['city'];
+value = user[prop];
+value = user[prop][0];
+
+user.firstName = 'Den';
+
+value = user.firstName;
+
+user.info = 'Some info';
+
+user['user-address'].city = 'Kiev';
+user['user-address'].contry = 'Ukraine';
+
+console.log(user.plan);
+
+user.plan = {};
+user.plan.basic = 'basic';
 
 
 console.log(value);
+console.log(user);
