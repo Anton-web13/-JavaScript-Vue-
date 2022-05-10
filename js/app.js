e35a9caa6601c7243324dd4463a978172c921414
Anton <example@gmail.com>
2022-05-10 22:43:13 +0300

    Функции. Задачи. (до методов массива)

--- js/app.js
+++ js/app.js
@@ -1,106 +1,88 @@
-// function sayHello(firstName, lastName) {
-//     if (!firstName) return console.log('Error!');
-//     console.log(firstName, lastName);
-//     console.log("Hello world");
-//     return `Hello ${firstName} ${lastName}`;
+// let array = [1, 2 , 99];
+
+// function multiply(array) {
+
+//     console.log(array);
+
+//     let res = 0;
+
+//     if (array.length == 0) {
+//         return 0;
+//     } else if (array.length !== 0) {
+//         res = 1;
+//         for (let i = 0; i < array.length; i++) {
+//             res *= array[i];
+//         }
+//         return console.log(res);
+//     }
 // }
 
-// let res = sayHello("Denis", "Mescheryakov");
-// let res2 = sayHello("Dima", "Mescheryakov") + '!';
-// let res3 = sayHello();
-
-// // console.log(res);
-// // console.log(res2);
-// console.log(res3);
+// multiply(array);
 
 
 
 
-function sayHello(firstName = "Default", lastName = "Default") {
-    if (!firstName) return console.log('Error!');
-    console.log(firstName, lastName);
-    console.log("Hello world");
-    return `Hello ${firstName} ${lastName}`;
+// let string = 'test';
+// let revers = '';
+
+// function test(string) {
+
+//     // let revers = '';
+
+//     for (let i = string.length -1; i >= 0; i-- ) {
+//         console.log(string[i]);
+//         revers += string[i];
+//     }
+//     return console.log(revers);
+// }
+
+// test(string)
+
+
+
+
+
+
+
+// let string1 = 'hello';
+
+// let spliting = string1.split('');
+
+// function getCodeStringFromText(spliting) {
+//     console.log(spliting);
+
+//     for (let i = 0; i < spliting.length; i++) {
+//         // console.log(spliting[i]);
+//         spliting[i] = spliting[i].charCodeAt(0);
+//         console.log(spliting[i]);
+//     }
+
+//     let ret = spliting.join(' ');
+//     return console.log(ret);
+// }
+
+// getCodeStringFromText(spliting);
+
+
+
+
+let value = Math.round(Math.random()*10);
+
+let myZahl = 5;
+
+
+function zahl(value, myZahl) {
+    console.log(value);
+    if (value >= 0 && value <= 10) {
+
+        if (value == myZahl) {
+            return console.log('Вы выиграли!');
+        } else 
+        return console.log(`Вы не угадали! Ваше число ${myZahl} а выпало число ${value}`);
+        
+    } else {
+        return console.log('Fehler');
+    }
 }
 
-// let res = sayHello("Denis", "Mescheryakov");
-// let res2 = sayHello("Dima", "Mescheryakov") + '!';
-// let res3 = sayHello();
-// let res3 = sayHello('null');
-
-// console.log(res);
-// console.log(res2);
-// console.log(res3);
-
-
-
-
-// let x = 10;
-
-// function foo(x) {
-//     x = 20;
-//     console.log(x);
-//     return x;
-// }
-
-// foo();
-  
-// console.log(x);
-
-
-
-
-// const user = {
-//     name: "Denis",
-//     age: 30
-// };
-
-
-// function getObj(obj) {
-//     // console.log(obj);
-//     obj.name = "Den";
-// }
-
-// getObj(user);
-// console.log(user);
-
-
-
-
-const square = function(x) {
-    return x * x;
-};
-
-console.log(square(8));
-
-
-
-
-
-
-
-(function(msg) {
-
-    function sayHello(firstName = "Default", lastName = "Default") {
-        if (!firstName) return console.log('Error!');
-        console.log(firstName, lastName);
-        console.log("Hello world");
-        return `Hello ${firstName} ${lastName}`;
-    }
-
-    // let res = sayHello("Denis", "Mescheryakov");
-    // let res2 = sayHello("Dima", "Mescheryakov") + '!';
-    // let res3 = sayHello();
-    // let res3 = sayHello('null');
-
-    // console.log(res);
-    // console.log(res2);
-    // console.log(res3);
-
-    const square = function(x) {
-        return x * x;
-    };
-
-    console.log(square(8));
-
-})("Hello worldw");
+zahl(value, myZahl);
