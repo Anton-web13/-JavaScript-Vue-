4c02b7e87f4954a6641ce32c18f90b5a474c26cd
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-05-11 17:13:57 +0300

    30. Методы массивов

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
-// function foo(x) {
-//     console.log(x);
-//     console.log(arguments);
+const numArr = [2, 32, 1234, 54, 323];
 
-//     for (let i = 0; i < arguments.length; i++) {
-//         console.log(arguments[i]);
-//     }
-// }
+console.log(numArr);
 
-// foo(11, "some string", [1, 2, 3]);
+let value;
 
+value = numArr.length;
 
+// numArr.length = 0;
+// numArr.length = 100;
 
+value = Array.isArray(numArr);
+value = numArr[2];
+numArr[2] = 12;
+value = numArr.indexOf(32);
 
+value = numArr.push(100);  // hinzufügen am Ende
+value = numArr.pop();  // löschen am Ende
+value = numArr.unshift(111);  // hinzufügen am Anfang
+value = numArr.shift();  // löschen am Anfang
+value = numArr.slice(0, 2);  // ausschneiden aus dem Array
+value = numArr.slice(); // die Kopie vom Array
+value = numArr.splice(1, 2, "one", "two");
+// value = numArr.splice(1, 0, "one", "two");
+// value = numArr.splice(1, 2);
+// value = numArr.reverse();
+// value = numArr.concat(numArr);
+value = numArr.concat(1, 2); // hinzufügen am Ende
+// value = [].concat(1, 2, 3);
+value = numArr.join(" ");
+value = "hello world".split(" ");
 
-// Функции. Возвращаемое значение. Return.
-
-function foo() {
-    let a = 5; 
-    return a * 2;
-}
-
-let result = foo();
-
-console.log(result);
+console.log(value, numArr);
