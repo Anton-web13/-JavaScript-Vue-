a5b09a8acaa50ff485ba8a4a14e48c5e97080930
Anton <example@gmail.com>
2022-05-11 22:25:15 +0300

    Функции. Задачи. После методов. Часть 2

--- js/app.js
+++ js/app.js
@@ -37,21 +37,78 @@
 
 
 
-function nnnn (n) {
-    n = 3;
-    let num = [];
+// function nnnn (n) {
+//     n = 3;
+//     let num = [];
     
-    for (let i = 1; i <= n; i++) {
+//     for (let i = 1; i <= n; i++) {
 
-        num.push(i);
-        num.concat(num);
+//         num.push(i);
+//         num.concat(num);
+//     }
+//     return num;
+// }
+
+// let rew = nnnn();
+
+// console.log(rew);
+
+// let tyt = rew.concat(rew);
+// console.log(tyt);
+
+// let array = [1,2,3];
+// let array2 = ['a','b','c'];
+
+// function nnnn () {
+//     // console.log(array);
+//     console.log(arguments);
+    
+//     for (let i = 0; i < arguments.length; i++) {
+//         // console.log(arguments[i]);
+//         let nummer = arguments[i].splice(1, 2);
+//         console.log(nummer);
+//     }
+// }
+
+// nnnn(array, array2);
+
+
+
+const users = [
+    {name: 'Pavel', gender: 'male', age: 29},
+    {name: 'Anna', gender: 'female', age: 18},
+    {name: 'Fiona', gender: 'female', age: 19},
+    {name: 'Julia', gender: 'female', age: 28},
+    {name: 'Alex', gender: 'male', age: 23},
+    {name: 'Sonya', gender: 'female', age: 17},
+    {name: 'Frank', gender: 'male', age: 44},
+    {name: 'John', gender: 'male', age: 49},
+    {name: 'Greta', gender: 'female', age: 57}
+];
+
+function funcGetUsers(arrayUsers, key, keyValue){
+    let result = [];
+    for (let user of arrayUsers) {
+        // console.log(arrayUsers);
+        // console.log(user);
+        // console.log(user[key]);
+        if (user[key] === keyValue) {
+            result.push(user);
+        }
     }
-    return num;
-}
+    return result;
+};
 
-let rew = nnnn();
+const newUser = funcGetUsers(users, 'gender', 'female');
+console.log(newUser);
 
-console.log(rew);
 
-let tyt = rew.concat(rew);
-console.log(tyt);
+// const user = {
+//   name: 'Denis',
+//   age: 30
+// };
+
+// for (let kot in user) {
+//     console.log(kot);
+//     console.log(user[kot]);
+// }
