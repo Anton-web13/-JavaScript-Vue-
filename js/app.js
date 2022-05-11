d4d9985a16fe45d2356b13e080fbfb84d98501a6
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-05-11 15:56:51 +0300

    24. Дополнение к лекции по функциям

--- js/app.js
+++ js/app.js
@@ -1,88 +1,10 @@
-// let array = [1, 2 , 99];
+function foo(x) {
+    console.log(x);
+    console.log(arguments);
 
-// function multiply(array) {
-
-//     console.log(array);
-
-//     let res = 0;
-
-//     if (array.length == 0) {
-//         return 0;
-//     } else if (array.length !== 0) {
-//         res = 1;
-//         for (let i = 0; i < array.length; i++) {
-//             res *= array[i];
-//         }
-//         return console.log(res);
-//     }
-// }
-
-// multiply(array);
-
-
-
-
-// let string = 'test';
-// let revers = '';
-
-// function test(string) {
-
-//     // let revers = '';
-
-//     for (let i = string.length -1; i >= 0; i-- ) {
-//         console.log(string[i]);
-//         revers += string[i];
-//     }
-//     return console.log(revers);
-// }
-
-// test(string)
-
-
-
-
-
-
-
-// let string1 = 'hello';
-
-// let spliting = string1.split('');
-
-// function getCodeStringFromText(spliting) {
-//     console.log(spliting);
-
-//     for (let i = 0; i < spliting.length; i++) {
-//         // console.log(spliting[i]);
-//         spliting[i] = spliting[i].charCodeAt(0);
-//         console.log(spliting[i]);
-//     }
-
-//     let ret = spliting.join(' ');
-//     return console.log(ret);
-// }
-
-// getCodeStringFromText(spliting);
-
-
-
-
-let value = Math.round(Math.random()*10);
-
-let myZahl = 5;
-
-
-function zahl(value, myZahl) {
-    console.log(value);
-    if (value >= 0 && value <= 10) {
-
-        if (value == myZahl) {
-            return console.log('Вы выиграли!');
-        } else 
-        return console.log(`Вы не угадали! Ваше число ${myZahl} а выпало число ${value}`);
-        
-    } else {
-        return console.log('Fehler');
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
     }
 }
 
-zahl(value, myZahl);
+foo(11, "some string", [1, 2, 3]);
