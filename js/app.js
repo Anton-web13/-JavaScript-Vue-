306b9f9f09e3b1b18e8b37c93bc28feb53c2119b
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-06-01 10:49:29 +0300

    44. Перебирающие методы массивов

--- js/app.js
+++ js/app.js
@@ -1,170 +1,223 @@
-// const plus = (x, y) => console.log(x + y);
+const users = [
+    {
+      _id: "5cdce6ce338171bb473d2855",
+      index: 0,
+      isActive: false,
+      balance: 2397.64,
+      age: 39,
+      name: "Lucile Finley",
+      gender: "female",
+      company: "ZOXY",
+      email: "lucilefinley@zoxy.com",
+      phone: "+1 (842) 566-3328",
+      registered: "2015-07-12T09:39:03 -03:00"
+    },
+    {
+      _id: "5cdce6ce0aa8d071fa4f4cc5",
+      index: 1,
+      isActive: true,
+      balance: 2608.48,
+      age: 33,
+      name: "Woodward Grimes",
+      gender: "male",
+      company: "FORTEAN",
+      email: "woodwardgrimes@fortean.com",
+      phone: "+1 (960) 436-3138",
+      registered: "2014-09-08T03:24:39 -03:00"
+    },
+    {
+      _id: "5cdce6ce103de120d32d6fe4",
+      index: 2,
+      isActive: true,
+      balance: 1699.99,
+      age: 25,
+      name: "Robinson Coleman",
+      gender: "male",
+      company: "GENMOM",
+      email: "robinsoncoleman@genmom.com",
+      phone: "+1 (852) 543-3171",
+      registered: "2019-04-23T08:24:58 -03:00"
+    },
+    {
+      _id: "5cdce6cebada7a418d8ccb3d",
+      index: 3,
+      isActive: true,
+      balance: 2621.84,
+      age: 25,
+      name: "Austin Benton",
+      gender: "male",
+      company: "ZILIDIUM",
+      email: "austinbenton@zilidium.com",
+      phone: "+1 (977) 573-2627",
+      registered: "2016-08-02T10:08:24 -03:00"
+    },
+    {
+      _id: "5cdce6ced81fe99596d9cef5",
+      index: 4,
+      isActive: true,
+      balance: 1297.31,
+      age: 37,
+      name: "Casandra Stout",
+      gender: "female",
+      company: "ANACHO",
+      email: "casandrastout@anacho.com",
+      phone: "+1 (929) 465-3804",
+      registered: "2018-04-14T11:27:26 -03:00"
+    },
+    {
+      _id: "5cdce6ce6c3ae6c4d6f39e88",
+      index: 5,
+      isActive: false,
+      balance: 2165.49,
+      age: 20,
+      name: "Valencia Carrillo",
+      gender: "male",
+      company: "XEREX",
+      email: "valenciacarrillo@xerex.com",
+      phone: "+1 (977) 522-3378",
+      registered: "2014-02-14T11:45:27 -02:00"
+    }
+];
 
-// function plusFoo(x, y) {
-//     return x + y;
-// }
-// const plus = (x, y) => x + y;
-const plus = (x = 0, y = 0) => x + y;
-const plusRes = plus(1, 2);
-console.log(plusRes);
+// forEach
+// filter       фильтрует и возвращает новый массив
+// map          возвращает новый массив из колбэка
+// reduce       
+// some/every   проверка на соответствие
+// sort         сортировка
+// find         поиск
 
+
+
+///////////////////////////////////////////////////////// forEach
+
+// users.forEach((user) => {
+//     console.log(user)
+// })
+
+users.forEach((user, i, arr) => {
+    console.log(user, i, arr)
+})
+
+
+
+
+
+
+///////////////////////////////////////////////////////// filter
 console.clear();
 
-const withoutArgs = () => console.log('Hello world');
-const singleArg = x => x * 2;
-// const singleArg = (x = 1) => x * 2;
-// const plusResw = singleArg(4);
-// console.log(plusResw);
+// const userLess30 = users.filter((user, i, arr) => {})
 
-const moreActions = (a, b) => {
-    console.log(arguments);       // not defined
-    a *= 2;
-    b *= 3;
-    return a + b;   // return summa
-}
-
-
-// const returnObj = (str = '') => {
-//     return {
-//         value: str,
-//         length: str.length
-//     };
-// };
-
-const returnObj = (str = '') => ({
-    value: str,
-    length: str.length
-});
-
-
-function plusFoo(x, y) {
-    console.log(arguments);   // Object
-    return x + y;
-}
-
-// plusFoo(1, 2, 3, 'hello');
-
-
-// const obj = {
-//     firstName: "Denis",
-//     age: 30,
-//     getFirstName() {
-//         console.log(this);
-//     },
-//     // getAge: () => console.log(this),
-//     getAgeArrow: null,
-//     getAge() {
-//         // this.getAgeArrow = () => console.log(this);
-//         setTimeout(() => console.log(this))
-//     }
-// };
-
-// obj.getFirstName();
-// obj.getAge();         // 1) присвоение
-// obj.getAgeArrow();      // 2) выполнение
-
-
-
-
-
-//////////////////////////// 1)
-
-// function sum() {
-//     const params = Array.prototype.slice.call(arguments);
-//     // return params;
-  
-//     // if (!params.length) {
-//     //     return console.log(0);
-//     // } else {
-//     //     return console.log(1);
-//     // }
-
-//     if (!params.length) return console.log(0);
-
-//     // if (params.length) return console.log(1);
-    
-//     return params.reduce(function (prev, next) { return prev + next; });
-// }
-// let result = sum(1, 2, 3, 5, 8);
-
-// console.log(result);
-
-
-
-// const sum = (...params) => {
-//     if (!params.length) return console.log(0);
-//     if (params.length) return params.reduce((prev, next) => prev + next);
-//     // return params.reduce((prev, next) => prev + next);
-// }
-// console.log(sum(1, 2, 3, 5, 8));
-
-
-const sum = (...params) => {
-    console.log(params);
-}
-sum(1, 2, 3, 5, 8);
-
-
-
-// // let list2 = [1, 2, 3, 5, 8];
-// let one = [1, 2, 3, 5, 8].reduce((prev, next) => prev + next);
-// console.log(one)
-
-
-
-
-
-// let list2 = [1, 2, 3, 5, 8];
-// let one = list2.reduce((prev, next) => {
-//     if (!list2.length) return 0;
-//     if (list2.length) return prev + next;
-// });
-// console.log(one);
-
-
-
-
-
-// console.clear();
-// let list1 = sum(1, 2, 3); // [1, 2, 3]
-// console.log(list1);
-
-
-
-
-// let list2 = [1, 2, 3, 5, 8];
-// // let initialValue = 1;
-// let qwe = list2.reduce(function(prev, next) {
-//     return prev + next;
-// }, 1)
-// console.log(qwe);
-
-
-
-
-
-// let qwe2 = list2.reduce(function(previousValue, currentValue, index, array) {
-//     return previousValue + currentValue;
+// const userLess30 = users.filter((user) => {
+//     return user.age < 30;
 // });
 
-// console.log(qwe2);
+// const userLess30 = users.filter((user) => user.age < 30);
+// const userLess30 = users.filter((user) => user.isActive);
+const userLess30 = users.filter((user) => !user.isActive);
+console.log(userLess30);
 
 
 
 
 
-// [0,1,2,3,4,5,6,7].sort((prev, next) => {
-//     console.log(prev + next);
-//     // console.log(prev);
+///////////////////////////////////////////////////////// map
+console.clear();
+
+// const usersName  = users.map((user, i, arr) => console.log(user, i, arr));
+// const usersName  = users.map(user => user.name);
+const usersName  = users.map(user => ({name: user.name, age: user.age}));
+
+console.log(usersName);
+
+
+
+
+
+///////////////////////////////////////////////////////// reduce
+console.clear();
+
+// const totalBalance = users.reduce((acc, user, i, arr) => {
+//     console.log(acc, user, i, arr);
+// }, 0);  
+
+// const totalBalance = users.reduce((acc, user) => {
+//     console.log(acc);
+//     return acc += user.balance;
+// }, 0); 
+// acc - результат предыдущей итерации
+
+const totalBalance = users.reduce((acc, user) => (acc += user.balance), 0); 
+// console.log(totalBalance);
+const usersObj = users.reduce((acc, user) => {     // из массива создаем объект
+    acc[user._id] = user;
+    return acc;
+}, {});
+console.log(usersObj);
+
+
+
+
+
+
+///////////////////////////////////////////////////////// some/every
+console.clear();
+
+// const isMale = users.some((user, i, arr) => {});
+
+const isMale = users.some(user => user.gender === 'male');  //  хоть один - тогда true or false
+const isAllMale  = users.every(user => user.gender === 'male');  // для каждого элемента
+const isAll18 = users.every(user => user.age > 18);   // true
+console.log(isAll18);
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////// find
+console.clear();
+
+// const user = users.find((user, i, arr) => {});
+const user = users.find(user => user.name === 'Valencia Carrillo');
+console.log(user);
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////// Sort
+console.clear();
+
+// изменяет массив
+// сортирует как строки
+
+const strArr = ["Denis", "Bill", "Anna"];
+console.log(strArr);                            // ['Denis', 'Bill', 'Anna']
+strArr.sort();
+console.log(strArr);                            // ['Anna', 'Bill', 'Denis']
+
+const numArr = [10, 7, 44, 32];
+numArr.sort();
+// console.log(numArr);                          // [10, 32, 44, 7]
+
+// numArr.sort((prev, next) => {
+//     console.log(prev, next);                     // [10, 32, 44, 7]
 // });
 
+// console.log(numArr); 
 
 
+// numArr.sort((prev, next) => prev - next);       // [7, 10, 32, 44]
+// numArr.sort((prev, next) => prev + next);       //  [10, 32, 44, 7]
 
+// console.log(numArr); 
 
-// const test = () => { 
-//     const pi = Math.PI;
-//     return console.log(pi * 2); 
-// }
+users.sort((prevUser, nextUser) => prevUser.age - nextUser.age);   // сортировка объектов
+console.log(users); 
 
-// test();
