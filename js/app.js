35feab5c321fa79dfefd9723c8701cc42d987f87
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-06-22 14:37:04 +0300

    1. Введение в DOM

--- js/app.js
+++ js/app.js
@@ -1,101 +1,30 @@
-console.log('Request data...');
+const div = document.querySelector('div');
+const titles = document.querySelectorAll('h1');
+// const h1 = document.getElementsByTagName('h1');
+// console.dir(h1);
+console.log(titles);
 
-// const p = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log('Praparing data...');
-//         const backendData = {
-//             server: 'aws',
-//             port: 2000,
-//             status: 'working'
-//         }
-//         resolve(backendData);
-//     }, 2000)
-// })
-
-// p.then((data) => {
-//     console.log('Promise resolved', data);
-// })
+// console.log(Array.from(titles));
+// console.log(Array.prototype.slice.call(titles));    // sehr alt
+// console.log([...titles]);
 
 
+console.log(div);
+console.log(div.nextSibling);          // следующий узел после div (здесь это текст)
+console.log(div.nextElementSibling);   // следующий элемень(тэг) после div
+console.log(div.firstChild);           // первый текстовый узел в div
+console.log(div.firstElementChild);    // первый дочерний элемент в div (это ссылка)
 
-// p.then((data) => {
-//     const p2 = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             data.modified = true;
-//             resolve(data);
-//         }, 2000)
-//     })
+// const element = document.body.firstChild;   // #text
+// const element = document.body.firstChild.nextSibling;   // <!-- my comment -->
+// const element = document.body;   // целый документ в body
+// const element = document.links;   // все ссылки на странице
+const element = document.forms;   // все формы на странице
 
-//     p2.then((clientData) => {
-//         console.log('Data received', clientData);
-//     })
-// })
+console.clear();
 
-
-
-// p.then(data => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             data.modified = true;
-//             resolve(data);
-//         }, 2000)
-//     })
-// }).then(clientData => {
-//     // console.log('Data received', clientData);
-//     // clientData.fromPromise = true;
-//     // return clientData;
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log('Data received', clientData);
-//             clientData.fromPromise = true;
-//             resolve(clientData);
-//         }, 2000)
-//     })
-// })
-//     .then((clientData2) => {
-//         setTimeout(() => {
-//             console.log('Data finshed', clientData2);
-//         }, 2000)
-//     })
-//     .catch(err => console.error('Error: ', err))
-//     .finally(() => console.log('Finally'));
-
-
-
-
-
-
-
-const sleep = ms => {
-    return new Promise(resolve =>{
-        setTimeout(() => resolve(), ms)
-    })
-}
-
-// function sleep(ms) {
-//     return new Promise(resolve =>{
-//         setTimeout(() => resolve(), ms)
-//     })
-// }
-
-// const sfasf = function sleep(ms) {
-//     return new Promise(resolve =>{
-//         setTimeout(() => resolve(), ms)
-//     })
-// }
-
-// sleep(2000).then(() => console.log('After 2 sec.'))
-// sleep(3000).then(() => console.log('After 3 sec.'))
-
-
-
-Promise.all([sleep(2000), sleep(5000)]).then(() => {     // выполнение после 2-ого промиса
-    console.log('All promises')
-})
-
-Promise.race([sleep(2000), sleep(5000)]).then(() => {     // выполнение после 1-ого промиса
-    console.log('Race promises')
-})
-
-
+console.log(element);
+console.log(window);
+console.log(navigator);
+console.log(location);
 
