82866097e4cb1d279f2cc89b0489001f3e900ecf
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-06-27 15:16:10 +0300

    Рекурсия

--- js/app.js
+++ js/app.js
@@ -1,153 +1,168 @@
-const div = document.querySelector('div');
-const titles = document.querySelectorAll('h1');
-// const h1 = document.getElementsByTagName('h1');
-// console.dir(h1);
-console.log(titles);
-
-// console.log(Array.from(titles));
-// console.log(Array.prototype.slice.call(titles));    // sehr alt
-// console.log([...titles]);
-
-
-console.log(div);
-console.log(div.nextSibling);          // следующий узел после div (здесь это текст)
-console.log(div.nextElementSibling);   // следующий элемень(тэг) после div
-console.log(div.firstChild);           // первый текстовый узел в div
-console.log(div.firstElementChild);    // первый дочерний элемент в div (это ссылка)
-
-// const element = document.body.firstChild;   // #text
-// const element = document.body.firstChild.nextSibling;   // <!-- my comment -->
-// const element = document.body;   // целый документ в body
-// const element = document.links;   // все ссылки на странице
-const element = document.forms;   // все формы на странице
-
-console.clear();
-
-console.log(element);
-console.log(window);
-console.log(navigator);
-console.log(location);
-
-console.log(div.parentElement);         // ссылка на родителя для этого div
-
-console.clear();
-
-const link = div.querySelector('.link');
-
-// console.log(link.parentElement);        // прямой родитель
-// console.log(link.closest('.content')); // ближайший родитель с указаным селектором
+// function pow(x, n) {
+//     if (n == 1) {
+//       return x;
+//     } else {
+//       return x * pow(x, n - 1);
+//     }
+//   }
+  
+//   console.log( pow(2, 3) ); // 8
 
 
-div.classList.add('article', 'custom');
-div.classList.remove('article');
-// console.log(div.classList);
-// console.log(div.classList.contains('custom'));
-// console.log(div.classList.toggle("toggle"));
-// console.log(div.classList);
-// console.log(div.classList.toggle("toggle"));
-// console.dir(link);
-// console.log(link.href);                             // найти адрес ссылки
 
+// рекурсия
+let t = 0;
 
-div.setAttribute('id', 'myId');                        // добавление аттрибутов
-console.dir(div);
-console.dir(div.id);
-
-console.log(div.getAttribute('id'));                   // получение аттрибутов
-
-// console.log(div.hasAttribute('id'));                   // true or false
-
-div.removeAttribute('id');                // удаление аттрибутов
-console.dir(div);
-
-console.dir(div.dataset);
-console.dir(div.dataset.myattr);          // myCustom value
-
-div.dataset.myattr = 'my a';
-console.dir(div.dataset);
-
-console.log(div.getAttribute('data-myattr'));      // получение аттрибутов
+function f1() {
+    t++;
+    console.log(t);
+    if (t === 100) return;
+    f1();
+}
+//   f1();
 
 
 
 
 
 
-console.clear();
-
-const title = document.querySelector('h1');
-// title.innerHTML = '<span>text</span>';    // <h1><span>text</span></h1>
-// title.textContent = '<span>new text</span>';  //<h1>&lt;span&gt;new text&lt;/span&gt;</h1>
-
-// title.appendChild('<span>appen</span>');      // kommt ein Fehler
-
-// title.insertAdjacentHTML('beforebegin', '<h2>title h2 </h2>');
-// title.insertAdjacentElement('afterbegin')
 
 
+// цикл
+function f2() {
+    let out = '';
+    for(let i = 1; i <= 30; i++) {
+        out += i + ' ';
+    }
+    console.log(out);
+}
 
-// ------------------------------------------------------------------------------------
+// f2();
 
-// innerHTML удалеят ссылки на обект и с ним нет никакой уже связи на странице. Объект уже не изменить.
-// Это и есть главная опасность innerHTML.
-// Поэтому лучше использовать appendChild, insertAdjacentHTML и insertAdjacentElement
-
-// ------------------------------------------------------------------------------------
-
-
-// title.innerHTML = '<span>new text123</span>';
-// title.innerHTML += '<span>new text123</span>';
-// const span = title.querySelector('span');
-// console.log(span);
-// title.innerHTML += '<span>new text456</span>';
-// span.textContent = 'aesfawerfas';
-
-
-
-// *Создание элемента
-
-const span = document.createElement('span');
-span.textContent = 'span created by createElement';
-span.classList.add('myClass', 'id');
-console.log(span);
-// title.appendChild(span);
-// title.insertAdjacentHTML('beforebegin', 'span created by createElement');
-// title.insertAdjacentHTML('afterbegin', 'Span created by createElement');
-
-
-
-
-// ------------------------------------------------------------------------------------
-// DOM узлы (span) могуть быть только в одном месте и в одном экземпляре
-
-title.appendChild(span);
-// div.appendChild(span);
-
-// ------------------------------------------------------------------------------------
+// рекурсия
+let i = 0;
+let out = '';
+function f3() {
+    i++;
+    out += i + ' ';
+    if (i >= 30) return;
+    f3();
+}
+// f3();
+// console.log(out);
 
 
 
 
 
-const fragment = document.createDocumentFragment();
-const colors = ['black', 'yellow', 'orange'];
-
-colors.forEach(color => {
-    const item = document.createElement('div');
-    item.style.background = color;
-    item.classList.add(`bg-${color}`);
-    item.textContent = color;
-    fragment.appendChild(item);
-});
-
-document.body.appendChild(fragment);
 
 
 
-// *Удаление элементов
+// рекурсия
+function randomInteger(min, max) {
+    let rand = min = Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
 
-// title.remove();
-// title.parentElement.removeChild(title);
+// randomInteger(2, 4);
+// console.log(randomInteger(2, 4));
+
+
+let s1 = 0;
+function moneyRecursion() {
+    let m = randomInteger(0, 100);
+    console.log('add: ' + m);
+    s1 += m;
+    console.log('sum: ' + s1);
+    if (s1 >= 300) return;
+    moneyRecursion();
+}
+// moneyRecursion();
+
+// цикл
+function moneyCycle() {
+    let s2 = 0;
+    // for(let i = 0; true; i++) {
+    //     let m = randomInteger(0, 100);
+    //     console.log('add: ' + m);
+    //     s2 += m;
+    //     console.log('sum: ' + s2);
+    //     if (s2 >= 300) return;
+    // }
+
+    while(true) {
+        let m = randomInteger(0, 100);
+        console.log('add: ' + m);
+        s2 += m;
+        console.log('sum: ' + s2);
+        if (s2 >= 300) return;
+    }
+}
+// moneyCycle();
+
+
+
+
+
+
+
+const users = {
+    'ivanov': {
+        age: 25,
+        parent: {
+            'ivanov-a': {
+                age: 45
+            },
+            'ivanov-b': {
+                age: 43,
+                parent: {
+                    'sergeev-a': {
+                        age: 88,
+                        parent: {
+                            'lionenko': {
+                                age: 33,
+                                parent: {
+                                    'petrov': {}
+                                }
+                            }
+                        }
+                    },
+                }
+            }
+        }
+    },
+    'kostenko': {
+        age: 56,
+        parent: {
+            'ignatenko': {
+
+            },
+            'sniezko': {
+                age: 45
+            }
+        }
+    }
+}
+
+// console.log(users);
+
+
+function userParentRecursion(obj) {
+    if (obj.parent !== undefined) {
+        for (let key in obj.parent) {
+            console.log(key);
+            userParentRecursion(obj.parent[key]);
+        }
+    }
+}
+// userParentRecursion(users.kostenko);
+
+
+for(let key in users) {
+    userParentRecursion(users[key]);
+}
+
+
 
 
 
