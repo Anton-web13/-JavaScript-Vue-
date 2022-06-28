7403dcacdb70c25c7658ae3d7da05cc15832fb6f
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-06-28 20:25:56 +0300

    Манипуляция DOM элементами. Задачи. Part 1

--- js/app.js
+++ js/app.js
@@ -1,173 +1,166 @@
-// function pow(x, n) {
-//     if (n == 1) {
-//       return x;
-//     } else {
-//       return x * pow(x, n - 1);
-//     }
-//   }
-  
-//   console.log( pow(2, 3) ); // 8
+
+// 1)------------------------------------------------------------------
+let paragraph = document.querySelector('p').textContent;
+console.log(paragraph);
 
 
+// 2)------------------------------------------------------------------
+let paragraphKnoten = document.querySelector('p');
 
-// рекурсия
-let t = 0;
-
-function f1() {
-    t++;
-    console.log(t);
-    if (t === 100) return;
-    f1();
-}
-//   f1();
-
-
-
-
-
-
-
-
-// цикл
-function f2() {
-    let out = '';
-    for(let i = 1; i <= 30; i++) {
-        out += i + ' ';
+function domKnoten(value) {
+    let domObject = {
+        nodeType: value.nodeType,
+        tagName: value.tagName,
+        childNodes: value.childNodes,
     }
-    console.log(out);
+    return domObject;
 }
 
-// f2();
-
-// рекурсия
-let i = 0;
-let out = '';
-function f3() {
-    i++;
-    out += i + ' ';
-    if (i >= 30) return;
-    f3();
-}
-// f3();
-// console.log(out);
+console.log(domKnoten(paragraphKnoten));
 
 
+// 3)------------------------------------------------------------------
+console.clear();
+let getUL = document.querySelector('ul');
+console.log(getUL);
 
+let yyy = [];
 
+function getTextFromUl(ul) {    
 
+    let ull = Array.from(ul.querySelectorAll('li'));
 
-
-
-// рекурсия
-function randomInteger(min, max) {
-    let rand = min = Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
-
-// randomInteger(2, 4);
-// console.log(randomInteger(2, 4));
-
-
-let s1 = 0;
-function moneyRecursion() {
-    let m = randomInteger(0, 100);
-    console.log('add: ' + m);
-    s1 += m;
-    console.log('sum: ' + s1);
-    if (s1 >= 300) return;
-    moneyRecursion();
-}
-// moneyRecursion();
-
-// цикл
-function moneyCycle() {
-    let s2 = 0;
-    // for(let i = 0; true; i++) {
-    //     let m = randomInteger(0, 100);
-    //     console.log('add: ' + m);
-    //     s2 += m;
-    //     console.log('sum: ' + s2);
-    //     if (s2 >= 300) return;
+    // for (let key in ull) {
+    //     let aaa = ull[key].textContent;
+    //     yyy.push(aaa);
     // }
+    // return yyy;
 
-    while(true) {
-        let m = randomInteger(0, 100);
-        console.log('add: ' + m);
-        s2 += m;
-        console.log('sum: ' + s2);
-        if (s2 >= 300) return;
+    let retur = [...ull].map(value => value.textContent);
+    return retur;
+
+}
+
+console.log(getTextFromUl(getUL));
+
+
+// 4)------------------------------------------------------------------
+console.clear();
+// let paragraphKneoten = document.querySelector('p').childNodes;
+// // console.log(paragraphKneoten);
+
+// let elements = Array.from(paragraphKneoten);
+// console.log(elements);
+
+// elements.forEach(el => {
+//     if (el.nodeName === '#text') {
+//       el.textContent = '<p>-text-</p>';
+//       }
+// });
+
+
+// 5.1)------------------------------------------------------------------
+console.clear();
+
+
+let ulData = document.querySelector('ul');
+let liData = Array.from(ulData.querySelectorAll('li'));
+console.log(liData);
+
+
+for (let key in liData) {
+    console.log(liData[key]);
+    liData[key].classList.add('list');
+
+}
+
+console.log(ulData);
+
+
+// 5.2)------------------------------------------------------------------
+console.clear();
+
+var link = Array.from(document.querySelector('body').childNodes);
+console.log(link);
+
+for (let key in link) {
+    if (link[key].localName  === 'a') {
+        console.log(link[key]);
+        link[key].id = 'link';
     }
 }
-// moneyCycle();
+console.log(link);
 
 
 
 
 
+// var linkBody = Array.from(document.querySelector('body').childNodes);
+// console.log(linkBody);
+// let user = linkBody.find((user => user.localName === 'a'));
+
+// console.log(user);
 
 
-const users = {
-    'ivanov': {
-        age: 25,
-        parent: {
-            'ivanov-a': {
-                age: 45
-            },
-            'ivanov-b': {
-                age: 43,
-                parent: {
-                    'sergeev-a': {
-                        age: 88,
-                        parent: {
-                            'lionenko': {
-                                age: 33,
-                                parent: {
-                                    'petrov': {}
-                                }
-                            }
-                        }
-                    },
-                }
-            }
-        }
-    },
-    'kostenko': {
-        age: 56,
-        parent: {
-            'ignatenko': {
 
-            },
-            'sniezko': {
-                age: 45
-            }
-        }
-    }
-}
-
-// console.log(users);
+// 5.3)------------------------------------------------------------------
+console.clear();
 
 
-function userParentRecursion(obj) {
-    if (obj.parent !== undefined) {
-        for (let key in obj.parent) {
-            console.log(key);
-            userParentRecursion(obj.parent[key]);
-        }
-    }
-}
-// userParentRecursion(users.kostenko);
+let ulDataUl = document.querySelector('ul');
+let liDataLi = Array.from(ulData.querySelectorAll('li'));
+console.log(liDataLi);
 
-
-for(let key in users) {
-    userParentRecursion(users[key]);
+for(let i = 0; i < liDataLi.length; i+=2) {
+    console.log(liDataLi[i]);
+    liDataLi[i].classList.add('item')
 }
 
 
+// 5.4)------------------------------------------------------------------
+console.clear();
+
+var linkA = Array.from(document.querySelectorAll('a'));
+console.log(linkA);
+
+for (let key in linkA) {
+    linkA[key].classList.add('custom-link');
+}
+
+console.log(linkA);
 
 
+// 6)------------------------------------------------------------------
+console.clear();
 
+// let divs = document.querySelector('div');
 
+let domElemen = document.querySelector('ul');
+console.log(domElemen);
 
+let createElement = document.createElement('li');
+createElement.classList.add('new-item');
+createElement.textContent = 'item';
 
+domElemen.appendChild(createElement);
+console.log(domElemen); 
 
+let li = Array.from(domElemen.querySelectorAll('li'));
+console.log(li);
+
+for (let key in li) {
+    li[key].classList.remove('list', 'item');
+}
+
+console.log(li);
+console.log(domElemen);
+
+let conter = 0;
+let textContent = '';
+for (let i = 0; i < li.length; i++) {
+    conter++
+    textContent = li[i].textContent;
+    li[i].textContent = `${textContent} ${conter}`;
+    console.log(li[i]);
+}
 
