cb85a10bfb161c621c98f7b425703bff35b9092c
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-07-15 12:58:53 +0300

    События. Задачи. Часть 1

--- js/app.js
+++ js/app.js
@@ -1,77 +1,71 @@
-const btn = document.querySelector('button');
-const link = document.querySelector('a');
-// console.log(link);
-// console.dir(btn);
-// btn.onclick = function() {           // nur einen click
-//     console.log('click');
-// }
+let btn_msg = document.getElementById('btn-msg');
 
-// btn.onclick = function() {
-//     console.log('click 2');
-// }
+// console.log(ulList);
+// btn_msg.style.backgroundColor = 'red';
+let ppp = document.querySelector('p');
 
-// btn.addEventListener('click', function() {      // kann mehr click leisten
-//     console.log('click');
-// })
+// 1)--------------------------------------------------------------------------------
 
-// // btn.addEventListener('click', function() {
-// //     console.log('click 2');
-// // })
-
-// btn.addEventListener('click', function(e) {
-//     console.log(e);
-// })
+btn_msg.addEventListener('click', e => {
+    alert(btn_msg.textContent);
+});
 
 
+// 2)--------------------------------------------------------------------------------
 
+btn_msg.addEventListener('mouseover', e => {
+    btn_msg.classList.add('red');
 
-// btn.addEventListener('click', (e) => {
-//     console.log(this);
-//     console.log(e);
-// })
-
-// function clickhandler(e) {
-//     e.preventDefault();
-//     console.log(this);
-//     console.log('click');
-// }
-
-// link.addEventListener('click', clickhandler);
-
-// link.removeEventListener('click', clickhandler);
-
-
-
-
-// let href = document.querySelector('a');
-// console.log(href);
-// let bodyw= document.querySelector('body');
-// btn.addEventListener('click', (e) => {
-//     const div = document.createElement('div');
-//     div.textContent = Math.random();
-//     div.classList = 'container';
-//     href.insertAdjacentElement('afterend', div);
-//     console.log(bodyw);
-// })
-
-const container = document.querySelector('.container');
-
-btn.addEventListener('click', (e) => {
-    const div = document.createElement('div');
-    const nestedBtn = document.createElement('button');
-    div.textContent = Math.random();
-    nestedBtn.textContent = 'button';
-    div.appendChild(nestedBtn);
-    container.appendChild(div);
+    btn_msg.addEventListener('mouseout', e => {
+        btn_msg.classList.remove('red');
+    })
 })
 
-container.addEventListener('click', e => {
-    // console.dir(e.target);
-    if (e.target.tagName === 'BUTTON') {
-        console.log('Button click');
+// 3)--------------------------------------------------------------------------------
+document.body.addEventListener('click', e => {
+    console.log(`${ppp.textContent} ${e.target.tagName}`);
+});
+
+
+// 4)--------------------------------------------------------------------------------
+
+let btn_generate = document.getElementById('btn-generate');
+
+let ul = document.querySelector('ul');
+let arrayLi = ul.querySelectorAll('li');
+let counter = arrayLi.length;
+console.log(counter);
+
+btn_generate.addEventListener('click', e => {
+    counter++;
+    let liNew = document.createElement('li');
+    let button = document.createElement('button');
+    liNew.textContent = `Item ${counter}`;
+    ul.appendChild(liNew);
+    // ul.insertAdjacentElement('beforeend', button);
+
+
+    // ul.addEventListener('click', e => {
+    //     if (e.target.tagName === "BUTTON") {
+    //         ul.removeChild(e.target);
+    //         // ul.removeChild(button);
+    //     }
+    // })
+
+    let ulAll = document.querySelector('ul');
+    let arrayLiAll = ulAll.querySelectorAll('li');
+    
+    ulAll.addEventListener('click', removeElement)
+
+    function removeElement(e) {
+        for (let i = 0; i < arrayLiAll.length; i++) {
+            if(e.target === arrayLiAll[i]) {
+                e.target.remove();
+                // arrayLiAll[i].remove();
+            }
+        }
     }
 })
-
 
 
 
